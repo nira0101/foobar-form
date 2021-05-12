4cba713e630cd6f3186c8d4d0612bbecb20f618f
nirajan <nira0101@stud.kea.dk>
2021-05-12 19:24:34 +0200

    fetched-images

--- main.js
+++ main.js
@@ -3,14 +3,11 @@ import './sass/style.scss'
 "use strict"
 
 
-const Beers = {
-    name: "",
-    category: "",
-    alc: "",
-    label: ""
-
-}
-
+const images = [
+    { id: 1, src: './public/elhefe.png', title: 'elhefe', description: 'beer' },
+    { id: 2, src: './public/githop.png', title: 'githop', description: 'beer' },
+   
+  ];
 function get() {
     fetch("https://foobardata.herokuapp.com/beertypes", {
       method: "get",
@@ -26,15 +23,11 @@ function get() {
      
      const template = document.querySelector("template").content;
   const copy = template.cloneNode(true);
+  const img_url =beer.label
   copy.querySelector(".name").textContent = beer.name;
   copy.querySelector(".cate").textContent = beer.category;
   copy.querySelector(".alc-lvl").textContent = beer.alc;
-
-
-  
-
-
-
+  copy.querySelector(".beer-img").src = img_url;
   document.querySelector("main").appendChild(copy);
   }
   get();
