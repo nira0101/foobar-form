2b7b70f3adcc724c31b821df38e3d6c74f1e27c2
nirajan <nira0101@stud.kea.dk>
2021-05-12 17:50:59 +0200

    initial-fetch-form

--- main.js
+++ main.js
@@ -3,3 +3,39 @@ import './sass/style.scss'
 "use strict"
 
 
+const Beers = {
+    name: "",
+    category: "",
+    alc: "",
+    label: ""
+
+}
+
+function get() {
+    fetch("https://foobardata.herokuapp.com/beertypes", {
+      method: "get",
+    })
+            
+      .then((e) => e.json())
+      .then((data) => data.forEach(showBeer));
+  }
+
+
+ function showBeer(beer){
+     console.log(beer)
+     
+     const template = document.querySelector("template").content;
+  const copy = template.cloneNode(true);
+  copy.querySelector(".name").textContent = beer.name;
+  copy.querySelector(".cate").textContent = beer.category;
+  copy.querySelector(".alc-lvl").textContent = beer.alc;
+
+
+  
+
+
+
+  document.querySelector("main").appendChild(copy);
+  }
+  get();
+
