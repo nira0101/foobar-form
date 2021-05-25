3be210bac2f1fd0be185fcb68102ce46fa92664e
nirajan <nira0101@stud.kea.dk>
2021-05-25 16:43:09 +0200

    basic data coding done

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ import './sass/style.scss'
 "use strict"
 window.addEventListener("DOMContentLoaded", init);
 
+const form = document.querySelector("#form")
+
 
 function init(){
   get();
@@ -17,13 +19,12 @@ function get() {
       .then((e) => e.json())
       .then((data) => data.forEach(showBeer));
   }
- 
 
 
  function showBeer(beer){
      console.log(beer)
     
-     const template = document.querySelector("template").content;
+  const template = document.querySelector("template").content;
   const copy = template.cloneNode(true);
   const img_url =beer.label
   copy.querySelector(".name").textContent = ('Name: ')+beer.name;
@@ -32,12 +33,12 @@ function get() {
   copy.querySelector(".beer-img").src = img_url;
 
   //add to cart
-  const amount = copy.querySelector(".next")
-  amount.addEventListener('click', ()=>{
+  const amount = copy.querySelector(".atc")
+  amount.addEventListener('click', (p)=>{
     const div = document.createElement('div')
     div.innerHTML = `
     <img src="${beer.label}" class="atc-img" >
-    <h3>${beer.name}</h3>
+    <h3 class = b_name>${beer.name}</h3>
     <h4>${beer.category}</h4>
     <button class="c">X</button>
   
@@ -52,7 +53,8 @@ function get() {
 
       
     `
-    //rmove the selected item
+   
+    //remove the selected item
     const close = div.querySelector(".c");
     close.addEventListener("click", ()=>{
 
@@ -87,9 +89,68 @@ else{
   })
 
 
+
  
     const content = document.querySelector('.checkbox_container');
-    document.querySelector(".confirm").addEventListener("click", displayModal);
+      //validate form
+    document.querySelector(".pay-confirm").addEventListener("click", ()=>{
+      console.log("clicked");
+      document.querySelector("#selected-beer").classList.remove("hidden");
+      document.querySelector(".pay-confirm").classList.add("hidden");
+      document.querySelector("main").classList.add("blurred");
+      document.getElementById("submit").addEventListener("click", (e) =>{
+        if (!form.elements.your_name.checkValidity()) {
+          document.querySelector("#error-2").classList.add("hidden");
+          document.querySelector("#error-3").classList.add("hidden");
+          document.querySelector("#error-4").classList.add("hidden");
+          document.querySelector("#error-1").classList.remove("hidden");
+        } else if (!form.elements.c_number.checkValidity()) {
+          document.querySelector("#error-1").classList.add("hidden");
+          document.querySelector("#error-3").classList.add("hidden");
+          document.querySelector("#error-4").classList.add("hidden");
+          document.querySelector("#error-2").classList.remove("hidden");
+        }else if (!form.elements.e_date.checkValidity()){
+          document.querySelector("#error-1").classList.add("hidden");
+          document.querySelector("#error-2").classList.add("hidden");
+          document.querySelector("#error-4").classList.add("hidden");
+          document.querySelector("#error-3").classList.remove("hidden");
+        } else if (!form.elements.s_number.checkValidity()){
+          document.querySelector("#error-1").classList.add("hidden");
+          document.querySelector("#error-2").classList.add("hidden");
+          document.querySelector("#error-3").classList.add("hidden");
+          document.querySelector("#error-4").classList.remove("hidden");
+        }else {
+          document.querySelector("#payment").classList.add("hidden");
+          document.querySelector("#confirm").classList.remove("hidden");
+          console.log("called")
+          
+          
+  const data = [
+    { name: beer.name , amount: 1},
+    
+  ];
+
+  const beerData =JSON.stringify(data);
+
+  fetch("https://foobardata.herokuapp.com/order", {
+  method: "post",
+  headers: {
+    "Content-Type" : "application/json; charset=utf-8",
+},
+  body: beerData
+  })
+  .then(e => e.json())
+  .then(data => console.log(data));
+       
+        }
+
+       
+      });
+    
+    document.querySelector("#close-2").addEventListener("click", closeForm);
+    document.querySelector("#close-3").addEventListener("click", closeForm);
+
+    });
    
     
     content.appendChild(div)
@@ -101,6 +162,10 @@ else{
   document.querySelector("main").appendChild(copy);
   
 
+ }
+
+ function postData(){
+ 
  }
 
 
@@ -131,49 +196,32 @@ else{
  
   
   
-/*  section Modal */
-  function displayModal(){
-    console.log("clicked");
-    document.querySelector("#selected-beer").classList.remove("hidden");
-    document.querySelector(".confirm").classList.add("hidden");
-    document.querySelector("main").classList.add("blurred");
 
-  document.querySelector("#next").classList.remove("hidden");
 
-  document.querySelector("#close-2").addEventListener("click", closeForm);
-  document.querySelector("#close-3").addEventListener("click", closeForm);
+function closeForm(){
+  console.log("ll")
 
-  document.querySelector("#back").addEventListener("click", backToPrevious);
-  document.querySelector('#back').addEventListener('click', confirm_payment);
-  document.querySelector("#previous").addEventListener("click", backToPrevious);
- /* I used the button back to next to confirm payment  */
-  }
-
-  function closeForm(){
-    console.log("ll")
-
-    document.querySelector("#selected-beer").classList.add("hidden");
-    document.querySelector(".confirm").classList.remove("hidden");
-    document.querySelector("main").classList.remove("blurred");
-    location.reload();
-  }
- function validateForm(){
-   console.log("vf")
-   document.querySelector("#modal-one").classList.add("hidden");
-   document.querySelector("#payment").classList.remove("hidden");
-   document.querySelector("button").classList.remove("hidden");
- }  
+  document.querySelector("#selected-beer").classList.add("hidden");
+  document.querySelector(".pay-confirm").classList.remove("hidden");
+  document.querySelector("main").classList.remove("blurred");
+  location.reload();
+}
+ 
 function backToPrevious(){
-  console.log("btp")
+console.log("btp")
 
-  document.querySelector("#payment").classList.add("hidden");
+document.querySelector("#payment").classList.add("hidden");
 }
 /* confirm payment section */
 function confirm_payment(){
-  document.querySelector("#payment").classList.add("hidden");
-  document.querySelector('#confirm').classList.remove('hidden');
+document.querySelector("#payment").classList.add("hidden");
+document.querySelector('#confirm').classList.remove('hidden');
 
- 
+
 }
 
- 
+
+
+
+
+form.setAttribute("novalidate", true);
